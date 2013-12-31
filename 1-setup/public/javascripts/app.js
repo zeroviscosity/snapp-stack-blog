7743c6fb74df9b07111447344aa3d47da0c10b29
Kent English <kent.english@gmail.com>
2013-12-31 08:24:42 -0500

    Removed About section from Part 1

--- 1-setup/public/javascripts/app.js
+++ 1-setup/public/javascripts/app.js
@@ -1,15 +1,3 @@
 'use strict';
 
 var app = angular.module('app', ['ngRoute', 'ngSanitize']);
-
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/', {
-        redirectTo: '/about'
-    }).when('/about', {
-        templateUrl: '/assets/templates/about.html'
-    }).otherwise({
-        redirectTo: '/'
-    });
-
-    $locationProvider.html5Mode(true).hashPrefix('!');
-}]);
