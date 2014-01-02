ca79f0a1f3d8a90ad2c0fa2139c38683202aa21a
Kent English <kent.english@gmail.com>
2014-01-02 09:06:38 -0500

    Added RSS feed

--- final/app/controllers/Application.scala
+++ final/app/controllers/Application.scala
@@ -4,7 +4,13 @@ import play.api._
 import play.api.mvc._
 
 import models.Entry
+import models.EntryFormat._
 import models.EntryStatus._
+
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+import scala.xml.XML._
 
 object Application extends Controller {
 
@@ -18,6 +24,44 @@ object Application extends Controller {
 
   def post(id: String) = Action {
     Ok(views.html.index())
+  }
+
+  def rssDescription(entry: Entry) = {
+    val pattern = """<h\d>([^<]+)<\/h\d>""".r
+    pattern findFirstIn entry.html match {
+      case Some(html) => loadString(html).text
+      case None => ""
+    }
+  }
+
+  def rss = Action {
+    val posts = Entry.findAllByFormat(Post, Published, 0, 10000)
+    val latest = posts.head
+    val format = "E, d MMM yyyy HH:mm:ss Z"
+    val pubDate = new SimpleDateFormat(format, Locale.CANADA).format(latest.created);
+    val rss =
+
+      <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+        <channel>
+          <title>Kent English</title>
+          <description>The Blog of Kent English</description>
+          <link>http://kentenglish.ca</link>
+          <lastBuildDate>{pubDate}</lastBuildDate>
+          <pubDate>{pubDate}</pubDate>
+          <ttl>1800</ttl>
+          <atom:link href="http://kentenglish.ca/rss.xml" rel="self" type="application/rss+xml" />
+          {for (post <- posts) yield {
+          <item>
+            <title>{post.title}</title>
+            <description>{rssDescription(post)}</description>
+            <link>http://kentenglish.ca/posts/{post.id}</link>
+            <guid>http://kentenglish.ca/posts/{post.id}</guid>
+            <pubDate>{new SimpleDateFormat(format, Locale.CANADA).format(post.created)}</pubDate>
+          </item>
+          }}
+        </channel>
+      </rss>
+    Ok(rss)
   }
 
   def sitemap = Action {
